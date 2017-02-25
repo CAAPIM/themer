ad1981ab196f2ec2b0cfff76767be8f0d5ab3395
Alessandro Biavati <abiavati@gmail.com>
2017-02-25 04:45:54 -0800

    test: convert all tests to jest

--- tests/Themer.spec.js
+++ tests/Themer.spec.js
@@ -4,15 +4,8 @@
  * of the MIT license. See the LICENSE file for details.
  */
 
-import chai from 'chai';
-import dirtyChai from 'dirty-chai';
-import sinon from 'sinon';
 import themerDecorator, { create, themer as themerInstance } from '../src';
 import Themer from '../src/Themer';
-
-// use dirty chai to avoid unused expressions
-chai.use(dirtyChai);
-const expect = chai.expect;
 
 let testInstance = null;
 
@@ -39,96 +32,96 @@ describe('Themer', () => {
     describe('default', () => {
       it('should export the result of Themer.render()', () => {
         const result = themerDecorator(snippet, testTheme)({ content: 'Hello' });
-        expect(result).to.equal('<h1 class="big-text-class">Hello</h1>');
+        expect(result).toBe('<h1 class="big-text-class">Hello</h1>');
       });
 
       it('should give access to the default instance of themer', () => {
-        expect(themerInstance instanceof Themer).to.be.true();
+        expect(themerInstance).toBeInstanceOf(Themer);
       });
     });
 
     describe('create', () => {
       it('should export create function that creates a new Themer instance', () => {
-        expect(create).to.be.a('function');
-        expect(create() instanceof Themer).to.be.true();
+        expect(typeof create).toBe('function');
+        expect(create()).toBeInstanceOf(Themer);
       });
 
       it('should allow for the creation of multiple Themer instances', () => {
         const testInstance1 = create();
         const testInstance2 = create();
 
-        expect(testInstance1.id).to.not.equal(testInstance2.id);
+        expect(testInstance1.id).not.toBe(testInstance2.id);
       });
     });
   });
 
   describe('middleware', () => {
     it('should accept middleware functions in the constructor options', () => {
-      const spy1 = sinon.spy();
-      const spy2 = sinon.spy();
+      const spy1 = jest.fn();
+      const spy2 = jest.fn();
 
       testInstance = create({ middleware: [spy1, spy2] });
-      expect(testInstance.getMiddleware().length).to.equal(2);
+      expect(testInstance.getMiddleware()).toHaveLength(2);
     });
 
     it('should provide a setter method to set middleware', () => {
-      const spy1 = sinon.spy();
-      const spy2 = sinon.spy();
+      const spy1 = jest.fn();
+      const spy2 = jest.fn();
 
       testInstance = create();
       testInstance.setMiddleware(spy1, spy2);
 
-      expect(testInstance.getMiddleware().length).to.equal(2);
+      expect(testInstance.getMiddleware()).toHaveLength(2);
     });
   });
 
   describe('theme', () => {
     it('should be an object', () => {
-      expect(themerInstance.theme).to.be.a('object');
+      expect(typeof themerInstance.theme).toBe('object');
     });
 
     it('should provide a setter method to set a theme', () => {
       testInstance = create();
 
-      expect(testInstance.setTheme).to.be.a('function');
+      expect(typeof testInstance.setTheme).toBe('function');
     });
 
     it('should provide a getter methods for theme id, variables, variants, and styles', () => {
       testInstance = create({ themes: [testTheme] });
 
-      expect(testInstance.getThemeId).to.be.a('function');
-      expect(testInstance.getThemeStyles).to.be.a('function');
-      expect(testInstance.getThemeVariables).to.be.a('function');
-      expect(testInstance.getVariants).to.be.a('function');
+      expect(typeof testInstance.getThemeId).toBe('function');
+      expect(typeof testInstance.getThemeStyles).toBe('function');
+      expect(typeof testInstance.getThemeVariables).toBe('function');
+      expect(typeof testInstance.getVariants).toBe('function');
     });
 
     it('accepts a non keyed array of theme attributes and adds styles key', () => {
       const nonKeyedTheme = { test: 'test' };
       testInstance = create({ themes: [nonKeyedTheme] });
 
-      expect(testInstance.getThemeId).to.be.a('function');
-      expect(testInstance.getThemeStyles).to.be.a('function');
-      expect(testInstance.getThemeVariables()).to.deep.equal({});
-      expect(testInstance.getVariants()).to.deep.equal({});
+      expect(typeof testInstance.getThemeId).toBe('function');
+      expect(typeof testInstance.getThemeStyles).toBe('function');
+      expect(testInstance.getThemeVariables()).toEqual({});
+      expect(testInstance.getVariants()).toEqual({});
     });
 
     it('will use theme1.styles if theme2.styles does not exist', () => {
       testInstance = create({ themes: [testTheme, {}] });
       const resolvedStyles = testInstance.getThemeStyles();
 
-      expect(resolvedStyles).to.deep.equal(testTheme.styles);
+      expect(resolvedStyles).toEqual(testTheme.styles);
     });
 
     it('should throw an error if theme is not an object or function', () => {
       testInstance = create();
-      expect(() => testInstance.setTheme([1])).to.throw();
+      expect(() => testInstance.setTheme([1])).toThrow();
     });
 
     it('should return an object of variants if supplied', () => {
       testInstance = create({ themes: [testTheme] });
       const variantObj = testInstance.getVariants();
       const expectedResult = { stop: false, go: false };
-      expect(variantObj).to.deep.equal(expectedResult);
+      expect(variantObj).toEqual(expectedResult);
     });
 
     it('should generated a new theme object when using .setTheme() on an existing Themer', () => {
@@ -136,7 +129,7 @@ describe('Themer', () => {
       const oldThemeId = testInstance.getThemeId();
       testInstance.setTheme([testTheme]);
 
-      expect(testInstance.getThemeId()).to.not.equal(oldThemeId);
+      expect(testInstance.getThemeId()).not.toBe(oldThemeId);
     });
 
     it('should accept theme.styles and theme.variables as functions', () => {
@@ -147,16 +140,16 @@ describe('Themer', () => {
 
       testInstance = create({ themes: [testFunctionTheme] });
       const renderedSnippet = testInstance.render(snippet, { content: 'Hello' });
-      expect(renderedSnippet).to.equal(`<h1 class="${testFunctionTheme.styles().root}">Hello</h1>`);
+      expect(renderedSnippet).toBe(`<h1 class="${testFunctionTheme.styles().root}">Hello</h1>`);
     });
 
     it('should return the resolved snippet and theme attributes', () => {
       testInstance = create();
       const resolvedAttrs = testInstance.resolveAttributes(snippet, [testTheme]);
 
-      expect(resolvedAttrs).to.be.a('object');
-      expect(resolvedAttrs.theme).to.deep.exist();
-      expect(resolvedAttrs.snippet).to.deep.exist();
+      expect(typeof resolvedAttrs).toBe('object');
+      expect(resolvedAttrs.theme).toBeTruthy();
+      expect(resolvedAttrs.snippet).toBeTruthy();
     });
 
     it('should use global variables when resolving theme variables and styles', () => {
@@ -175,10 +168,10 @@ describe('Themer', () => {
       const resolvedAttrs = testInstance.resolveAttributes(
         snippet, [testFunctionTheme], globalTheme.variables);
 
-      expect(resolvedAttrs).to.be.a('object');
-      expect(resolvedAttrs.theme).to.deep.exist();
-      expect(resolvedAttrs.snippet).to.deep.exist();
-      expect(resolvedAttrs.theme.styles).to.deep.equal(
+      expect(typeof resolvedAttrs).toBe('object');
+      expect(resolvedAttrs.theme).toBeTruthy();
+      expect(resolvedAttrs.snippet).toBeTruthy();
+      expect(resolvedAttrs.theme.styles).toEqual(
         testFunctionTheme.styles(null, globalTheme.variables)
       );
     });
@@ -192,7 +185,7 @@ describe('Themer', () => {
     testInstance = create({ themes: [testFuncTheme] });
     const resolvedStyles = testInstance.getThemeStyles();
 
-    expect(resolvedStyles).to.deep.equal(testFuncTheme.styles());
+    expect(resolvedStyles).toEqual(testFuncTheme.styles());
   });
 
   describe('render', () => {
@@ -200,24 +193,24 @@ describe('Themer', () => {
       testInstance = create({ themes: [testTheme] });
       const renderedSnippet = testInstance.render(snippet, { content: 'Hello' });
 
-      expect(renderedSnippet).to.equal('<h1 class="big-text-class">Hello</h1>');
+      expect(renderedSnippet).toBe('<h1 class="big-text-class">Hello</h1>');
     });
 
     it('should provide the rendered HTML snippet with styles resolved and mapped with variant support', () => {
       testInstance = create({ themes: [testTheme] });
       const renderedSnippet = testInstance.render(snippet, { content: 'Hello', stop: true, go: true });
 
-      expect(renderedSnippet).to.equal('<h1 class="big-text-class red-black-class green-grass-class">Hello</h1>');
+      expect(renderedSnippet).toBe('<h1 class="big-text-class red-black-class green-grass-class">Hello</h1>');
     });
 
     it('should run the middleware functions before render', () => {
-      const spy1 = sinon.spy((component) => component);
-      const spy2 = sinon.spy((component) => component);
+      const spy1 = jest.fn().mockImplementation((component) => component);
+      const spy2 = jest.fn().mockImplementation((component) => component);
       testInstance = create({ themes: [testTheme], middleware: [spy1, spy2] });
       testInstance.render(snippet);
 
-      expect(spy1.calledOnce).to.be.true();
-      expect(spy2.calledOnce).to.be.true();
+      expect(spy1).toHaveBeenCalled();
+      expect(spy2).toHaveBeenCalled();
     });
   });
 });
